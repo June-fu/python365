21651103e09c8c4a536049c5741ff8ff2501b1a5
june-fu <fujun1990@gmail.com>
2020-01-15 22:56:00 +0800

    two sum

--- january/01.py
+++ january/01.py
@@ -16,7 +16,7 @@ class Solution:
         num_to_index = {}
         for i, num in enumerate(nums):
             if target - num in num_to_index:
-                return [num_to_index[target - num],i]
+                return [num_to_index[target - num], i]
             else:
                 num_to_index[num] = i
         return
