78ee2af47a8704fd036ebe2f1a07344ee62a4b67
june-fu <fujun1990@gmail.com>
2020-02-01 00:22:45 +0800

    center align all lines in the given file

---
+++ january/27.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Author: june-fu
+# Date  : 2020/1/31
+"""
+Problem 23: Write a program center_align.py to center align all lines in the given file.
+input:python center_align.py she.txt
+  I'm sure that the shells are seashore shells.
+    So if she sells seashells on the seashore,
+The shells that she sells are seashells I'm sure.
+       She sells seashells on the seashore;
+"""
+
+
+def center_align(filename):
+    with open(filename) as f:
+        lst_file = f.readlines()
+        lst = [len(x) for x in lst_file]
+        max_len = max(lst)
+        middle = [int((max_len - y)/2 + y) for y in lst]
+
+        for i, line in zip(range(len(lst_file)), lst_file):
+            # this is the core
+            lst_file[i] = line[:-1].ljust(middle[i], ' ').rjust(max_len, ' ') + '\n'
+        return ''.join(lst_file)
+
+
+if __name__ == '__main__':
+    print(center_align('she.txt'))
