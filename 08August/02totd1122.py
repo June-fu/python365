3531082fbcc97479e8c73517a1cef6bee2077219
june-fu <june@yeah.net>
2020-11-22 12:52:35 +0800

    Anti-gravity

---
+++ 08August/02totd1122.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-11-22 12:42:09
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-11-22 12:42:20
+ # @ Description:
+ Anti-gravity
+ '''
+import antigravity
+
+antigravity.geohash
