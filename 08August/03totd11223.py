a8c7fa6db471870ec451fb0abf84cf1cafbf2ce4
june-fu <june@yeah.net>
2020-11-23 23:07:13 +0800

    The Zen of Python

---
+++ 08August/03totd11223.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-11-23 23:04:47
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-11-23 23:05:20
+ # @ Description:
+ The Zen of Python
+ '''
+import this
