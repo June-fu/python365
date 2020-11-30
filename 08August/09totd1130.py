c8593ba4dcdb001251066f1b7f607960350253df
june-fu <june@yeah.net>
2020-11-30 22:49:03 +0800

    List Slicing Tricks

---
+++ 08August/09totd1130.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-11-30 22:37:15
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-11-30 22:38:13
+ # @ Description:
+List Slicing Tricks
+ '''
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(x[0:3])
+print(x[3:])
+print(x[:6])
+print(x[0:6:2])
+print(x[:-3])
+print(x[::-1])
+print(x[-3::-1])
