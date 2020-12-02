4f13232b2668896217b30250c44860a0993df5a7
june-fu <june@yeah.net>
2020-12-02 23:23:53 +0800

    big number readability

---
+++ 08August/11totd1202.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-02 22:48:20
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-02 22:48:25
+ # @ Description:
+ big number readability
+ '''
+ 
+ # you can separate zeros with underscore(_)
+print(2_000_000)
+print(2_000_000 + 1)
