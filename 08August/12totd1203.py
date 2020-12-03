2e531c8a1e675d96004eca0820224488a3de5b94
june-fu <june@yeah.net>
2020-12-03 22:09:27 +0800

    chain operators

---
+++ 08August/12totd1203.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-03 22:07:36
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-03 22:08:16
+ # @ Description:
+chain operators
+ '''
+distance = 200
+checker = 100 < distance < 350
+
+print(checker)
