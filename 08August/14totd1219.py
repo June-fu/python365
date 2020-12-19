a8ee31760ee0459ee705020f886352387dcaa23b
june-fu <june@yeah.net>
2020-12-19 23:16:27 +0800

    Partial assignments

---
+++ 08August/14totd1219.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-19 23:14:52
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-19 23:14:55
+ # @ Description:
+ Partial assignments
+ '''
+
+lst = [5, 10, 20, 30]
+
+x, *y = lst
+print(x, y)
