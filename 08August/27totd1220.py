6af2ad7adfe9f32634dee12d40d3ce9fff2d7e15
june-fu <fujun1990@gmail.com>
2020-12-20 22:39:28 +0800

    Ordered Dictionaries

---
+++ 08August/27totd1220.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-20 22:36:56
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-20 22:37:00
+ # @ Description:
+ Ordered Dictionaries
+ '''
+
+import collections
+
+c = collections.OrderedDict()
+c[1] = 50
+c[2] = 100
+print(c)
