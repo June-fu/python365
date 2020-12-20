edcb84f0c2950c11a11d49794d0ef1fd7b30e959
june-fu <fujun1990@gmail.com>
2020-12-20 20:58:53 +0800

    Write a Pandas program to get the powers of an array values element-wise

---
+++ 11November/03PandasDataFrame01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-20 20:57:17
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-20 20:57:26
+ # @ Description:
+Write a Pandas program to get the powers of an array values element-wise
+'''
+
+import pandas as pd
+
+df = pd.DataFrame({'X':[78,85,96,80,86], 'Y':[84,94,89,83,86],'Z':[86,97,96,72,83]})
+print(df)
