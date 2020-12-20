200b3746df8c6d48d9d98bae2c8ebf3a08dfea10
june-fu <fujun1990@gmail.com>
2020-12-20 21:42:58 +0800

    df.iloc[:3]

---
+++ 08August/15PandasDataFrame04.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-20 21:37:46
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-20 21:38:50
+ # @ Description:
+ Write a Pandas program to get the first 3 rows of a given DataFrame
+ '''
+import pandas as pd
+import numpy as np
+
+dct1 = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(dct1, index=labels)
+print(df)
+
+result = df.iloc[:3]
+print("First three rows of the data frame:\n", result)
