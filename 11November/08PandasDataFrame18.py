6cd2c7e7903d355eae0df82ebc92d2ec49143c41
june-fu <fujun1990@gmail.com>
2020-12-21 22:40:26 +0800

    df['name'].replace('James', 'Suresh')

---
+++ 11November/08PandasDataFrame18.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2020-12-20 21:47:45
+ # @ Modified by: june-fu
+ # @ Modified time: 2020-12-21 22:07:54
+ # @ Description:
+Write a Pandas program to change the name 'James' to 'Suresh' in name column of the DataFrame
+'''
+import pandas as pd
+import numpy as np
+
+dct1 = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data=dct1, index=labels)
+print(df)
+
+df['name'] = df['name'].replace('James', 'Suresh')
+print(df)
