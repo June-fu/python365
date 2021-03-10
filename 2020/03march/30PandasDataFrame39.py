42657d7a63e59338f5f492a251b2f7123c2cdf18
june-fu <fujun1990@gmail.com>
2021-03-10 22:29:07 +0800

    combining two series into a DataFrame

--- 2020/03march/30PandasDataFrame39.py
+++ 2020/03march/30PandasDataFrame39.py
@@ -4,8 +4,14 @@
  # @ Create Time: 2021-03-10 22:16:03
  # @ Modified by: june-fu
  # @ Modified time: 2021-03-10 22:17:09
- # @ Description:
+ # @ Description:Write a Pandas program to combining two series into a DataFrame
  '''
 
 import pandas as pd
 import numpy as np
+
+s1 = pd.Series(['100', '200', 'python', '300.12', '400'])
+s2 = pd.Series(['10', '20', 'php', '30.12', '40'])
+
+df = pd.concat([s1, s2], axis=1)
+print("New DataFrame combining two series:\n", df)
