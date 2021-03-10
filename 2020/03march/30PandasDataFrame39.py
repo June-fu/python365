5fb9b29adfaae2d386df96219caf2f18bc19eced
june-fu <fujun1990@gmail.com>
2021-03-10 22:25:13 +0800

    df.sample()

---
+++ 2020/03march/30PandasDataFrame39.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''
+ # @ Author: june-fu
+ # @ Create Time: 2021-03-10 22:16:03
+ # @ Modified by: june-fu
+ # @ Modified time: 2021-03-10 22:17:09
+ # @ Description:
+ '''
+
+import pandas as pd
+import numpy as np
